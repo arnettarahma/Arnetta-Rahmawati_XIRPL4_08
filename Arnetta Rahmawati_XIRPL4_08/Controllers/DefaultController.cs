9ab2bb7b45b4cf1d52a53df7218f19b02dc4065b
Arnetta Rahma <arnettarahma@gmail.com>
2017-01-06 21:37:02 +0700

    Add project files.

---
+++ Arnetta Rahmawati_XIRPL4_08/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Arnetta_Rahmawati_XIRPL4_08.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Arnetta Rahmawati_XIRPL4_08/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arnetta_Rahmawati_XIRPL4_08.Global" Language="C#" %>
